82eadeab5c3fd8289f276777a6ff7842b1011125
Guilherme Daniel <gui.dani13@gmail.com>
2022-10-09 13:51:30 -0300

    criado enums

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+export enum NivelCurso{
+    tecnologo = "Tecnólogo",
+    medio_integrado = "Médio Integrado",
+    tecnico = "Técnico",
+    bacharel = "Bacharel",
+    licenciatura = "Licenciatura"
+}
+
+export enum TipoUsuario {
+    admin = "Administrador",
+    funcionario = "Funcionário",
+    aluno = "Aluno"
+}
+
+export enum CargoFuncionario {
+    professor = "Professor",
+    coordenador = "Coordenador"
+}
+
+export enum TipoOcupacao {
+    estagio = "Estágio",
+    iniciacaoCientifica = "Iniciação Científica",
+    efetivo = "Efetivo"
+}
