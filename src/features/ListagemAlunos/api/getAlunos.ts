5c04ebd22dea4a52ce0fc522bba403aa4dd28227
Guilherme Daniel <gui.dani13@gmail.com>
2022-10-13 08:19:50 -0300

    fixes

---
+++ src/features/ListagemAlunos/api/getAlunos.ts
@@ -0,0 +1,16 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../../database/firebase/config";
+
+export const getAlunos = async () => {
+  try {
+    let alunosLista = [];
+    const alunosSnapshot = await getDocs(collection(db, "ALUNOS"));
+    alunosSnapshot.forEach((aluno) => {
+        const alunoData = aluno.data();
+        console.log(alunoData)
+    });
+  } catch (error) {
+    // TODO: fazer tratamento de erros
+    console.log(error);
+  }
+};
---
+++ src/features/ListagemAlunos/components/ListaAlunos/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+const ListaAlunos = () => {
+  return (
+    <div>ListaAlunos</div>
+  )
+}
+
+export default ListaAlunos
