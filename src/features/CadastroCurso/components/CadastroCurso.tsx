09f9694ecf2d476cd6c34479b1071a8011e5fd1f
Guilherme Daniel <gui.dani13@gmail.com>
2022-12-04 13:58:19 -0300

    atualização página adicionar novo curso

--- src/features/CadastroCurso/components/CadastroCurso.tsx
+++ src/features/CadastroCurso/components/CadastroCurso.tsx
@@ -1,9 +1,15 @@
+import {
+  Button,
+  Center,
+  Container,
+  Divider,
+  Flex,
+  Heading,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { ICurso, NivelCurso } from "../../../types";
-import { ErrorMessage } from "../../ui/ErrorMessage";
-import Wrapper from "../../ui/wrapper";
 import { addCursoToDatabase } from "../api/addCursoToDatabase";
 
 const CadastroCurso = () => {
@@ -26,62 +32,65 @@ const CadastroCurso = () => {
   };
 
   return (
-    <>
-      <Wrapper>
-        <div className="novo-curso_form">
-          <div>
-            {loading && (<><h2>Aguarde...</h2></>)}
-          </div>
-          <form onSubmit={handleSubmit(submitForm)}>
-            <div className="form-input-section">
-              <h2>Dados do curso</h2>
-              <div className="input-group">
-                <label htmlFor="nomedocurso">Nome do curso*</label>
-                <input
-                  type="text"
-                  id="nomedocurso"
-                  placeholder="Nome do curso"
-                  {...register("nome", { required: true, minLength: 3 })}
-                />
-                {errors.nome && "Nome é obrigatório"}
-              </div>
-              {/*  */}
-              <div className="input-group">
-                <label htmlFor="niveldocurso">Nível do curso*</label>
-                <select
-                  id="niveldocurso"
-                  {...register("nivel", { required: true })}
-                >
-                  <option value="" disabled>
-                    --ESCOLHA--
-                  </option>
-                  <option value={NivelCurso.bacharel}>
-                    {NivelCurso.bacharel}
-                  </option>
-                  <option value={NivelCurso.licenciatura}>
-                    {NivelCurso.licenciatura}
-                  </option>
-                  <option value={NivelCurso.medio_integrado}>
-                    {NivelCurso.medio_integrado}
-                  </option>
-                  <option value={NivelCurso.tecnico}>
-                    {NivelCurso.tecnico}
-                  </option>
-                  <option value={NivelCurso.tecnologo}>
-                    {NivelCurso.tecnologo}
-                  </option>
-                </select>
-              </div>
+    <Container minW="full">
+      <Center>
+        <Heading fontSize="2xl">Novo curso</Heading>
+      </Center>
+      <Divider orientation="horizontal" m="2" />
+
+      <div className="novo-curso_form">
+        <form onSubmit={handleSubmit(submitForm)}>
+          <div className="form-input-section">
+            <div className="input-group">
+              <label htmlFor="nomedocurso">Nome do curso*</label>
+              <input
+                type="text"
+                id="nomedocurso"
+                placeholder="Nome do curso"
+                {...register("nome", { required: true, minLength: 3 })}
+              />
+              {errors.nome && "Nome é obrigatório"}
             </div>
             {/*  */}
-            <button type="submit">Cadastrar</button>
+            <div className="input-group">
+              <label htmlFor="niveldocurso">Nível do curso*</label>
+              <select
+                id="niveldocurso"
+                {...register("nivel", { required: true })}
+              >
+                <option value="" disabled>
+                  --ESCOLHA--
+                </option>
+                <option value={NivelCurso.bacharel}>
+                  {NivelCurso.bacharel}
+                </option>
+                <option value={NivelCurso.licenciatura}>
+                  {NivelCurso.licenciatura}
+                </option>
+                <option value={NivelCurso.medio_integrado}>
+                  {NivelCurso.medio_integrado}
+                </option>
+                <option value={NivelCurso.tecnico}>{NivelCurso.tecnico}</option>
+                <option value={NivelCurso.tecnologo}>
+                  {NivelCurso.tecnologo}
+                </option>
+              </select>
+            </div>
+          </div>
+          {/*  */}
+          <Flex gap="4">
+            <Button colorScheme="green" type="submit">
+              Cadastrar
+            </Button>
             <Link to="/">
-              <button type="reset">Cancelar</button>
+              <Button colorScheme="red" type="reset">
+                Cancelar
+              </Button>
             </Link>
-          </form>
-        </div>
-      </Wrapper>
-    </>
+          </Flex>
+        </form>
+      </div>
+    </Container>
   );
 };
 
