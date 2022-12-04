7d6ec314c17a5959804fcd6a5039f7a3f54d5a22
Guilherme Daniel <gui.dani13@gmail.com>
2022-12-04 19:01:51 -0300

    adicionado fonte roboto

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,24 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./features/auth/context/AuthProvider";
 import { Router } from "./router";
 import "./styles/globalStyles.css";
+import '@fontsource/roboto'
+
+const theme = extendTheme({
+  fonts: {
+    body: `'Roboto','sans-serif'`,
+  }
+})
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
         <AuthProvider>
-          <ChakraProvider>
+          <ChakraProvider theme={theme}>
             <Router />
           </ChakraProvider>
         </AuthProvider>
